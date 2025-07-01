6153a94ff77eb4ad14250c912fb90bc9e4da071c
sameerabdullahalgo <sameer.abdullah@algo.com>
2025-07-02 02:00:13 +0500

    added the user authentication and authorization flow

--- Bidding/Models/User.cs
+++ Bidding/Models/User.cs
@@ -6,6 +6,7 @@
         public int UserId { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
+        public string? Name { get; set; }
         public string LastName { get; set; }
         public string IdentificationNumber { get; set; }
         public string Address1 { get; set; }
